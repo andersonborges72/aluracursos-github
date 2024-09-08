6dcc31e8a770e4238322e07ee65dd71680dd48d8
andersonborges72 <152720575+andersonborges72@users.noreply.github.com>
2024-09-07 23:02:51 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+﻿function pesquisar() {
+  const section = document.getElementById("resultados-pesquisa");
+  const campoPesquisa = document
+    .getElementById("campo-pesquisa")
+    .value.toLowerCase();
+
+  if (campoPesquisa === "") {
+    section.innerHTML = "<p>Nada foi encontrado</p>";
+    return;
+  }
+
+  const resultados = [];
+  for (const dado of dados) {
+    if (
+      dado.titulo.toLowerCase().includes(campoPesquisa) ||
+      dado.descricao.toLowerCase().includes(campoPesquisa)
+    ) {
+      resultados.push(`
+        <div class="item-resultado">
+          <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `);
+    }
+  }
+
+  section.innerHTML = resultados.join("");
+}
---
+++ dados.js
@@ -0,0 +1,51 @@
+﻿let dados = [
+  {
+    titulo: "Alura",
+    descricao:
+      "A Alura é uma plataforma de ensino online que oferece uma ampla variedade de cursos e trilhas de aprendizagem nas áreas de programação, design e negócios.",
+    link: "https://www.alura.com.br/",
+  },
+
+  {
+    titulo: "Escola de Programação",
+    descricao:
+      "Mergulhe no universo back-end. Programe nas principais plataformas e linguagens, como Python Node.JS, PHP, Java e .NET. Crie sistemas web e desenhe a arquitetura de soluções inovadoras, com código e boas práticas.",
+    link: "https://www.alura.com.br/cursos-online-programacao",
+  },
+  {
+    titulo: "Escola de Front-end",
+    descricao:
+      "A Escola de Front-end é especializada em tecnologias para desenvolvimento de interfaces web.",
+    link: "https://www.alura.com.br/cursos-online-front-end",
+  },
+  {
+    titulo: "Escola de Data Science",
+    descricao:
+      "A Escola de Data Science oferece cursos sobre análise de dados, machine learning e inteligência artificial.",
+    link: "https://www.alura.com.br/cursos-online-data-science",
+  },
+  {
+    titulo: "Escola de Inteligência Artificial",
+    descricao:
+      "A Escola de Inteligência Artificial é focada em tecnologias de IA como aprendizado de máquina e deep learning.",
+    link: "https://www.alura.com.br/cursos-online-inteligencia-artificial",
+  },
+  {
+    titulo: "Escola de DevOps",
+    descricao:
+      "A Escola de DevOps ensina práticas e ferramentas para automatizar processos de desenvolvimento e infraestrutura.",
+    link: "https://www.alura.com.br/cursos-online-devops",
+  },
+  {
+    titulo: "Escola de Mobile",
+    descricao:
+      "A Escola de Mobile é especializada em desenvolvimento de aplicativos para dispositivos móveis.",
+    link: "https://www.alura.com.br/cursos-online-mobile",
+  },
+  {
+    titulo: "Escola de Inovação & Gestão",
+    descricao:
+      "A Escola de Inovação & Gestão oferece cursos sobre gestão de projetos, liderança e empreendedorismo.",
+    link: "https://www.alura.com.br/cursos-online-inovacao-gestao",
+  },
+];
